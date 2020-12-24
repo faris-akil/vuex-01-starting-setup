3b6d98be801aae5b8e4158ea1bab09196d177008
faris-akil <akil8989@gmail.com>
2020-12-24 13:38:25 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <base-container title="Vuex">
+    <h3>{{ $store.state.counter }}</h3>
+    <button @click="addOne">Add One</button>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+
+export default {
+  components: {
+    BaseContainer,
+  },
+  methods: {
+    addOne(){
+      this.$store.commit('increment')
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
